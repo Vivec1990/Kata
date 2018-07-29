60ec295662e2d5a3da115ecbd378959881426349
Vivec <meyer_sebastian@outlook.com>
2018-07-29 14:03:35 +0200

    Implementation of the main class for the scoring system and accompanying tests

--- BowlingKata/src/main/java/de/sebmey/kata/bowling/Bowling.java
+++ BowlingKata/src/main/java/de/sebmey/kata/bowling/Bowling.java
@@ -1,5 +1,55 @@
 package de.sebmey.kata.bowling;
 
+import java.util.List;
+
 public class Bowling {
+
+	public static final int FRAMES_PER_GAME = 10;
+	
+	private List<Frame> frames;
+	
+	public Bowling(String lineOfFrames) {
+		this.frames = FrameParser.parseLine(lineOfFrames);
+	}
+
+	public int calculateScore() {
+		int score = 0;
+		for(int i = 0; i < FRAMES_PER_GAME; i++) {
+			Frame currentFrame = frames.get(i);
+			if(currentFrame instanceof StandardFrame) {
+				score += currentFrame.getScore();
+			} else if(currentFrame instanceof SpareFrame) {
+				score += getTotalScoreForSpare(currentFrame, i);
+			} else if(currentFrame instanceof StrikeFrame) {
+				score += getTotalScoreForStrike(currentFrame, i);
+			}
+		}
+		return score;
+	}
+	
+	private int getTotalScoreForSpare(Frame spareFrame, int frameIndex) {
+		if(this.isLastFrame(frameIndex)) {
+			return spareFrame.getScore() + spareFrame.getBonusThrow();
+		}
+		Frame nextFrame = frames.get(frameIndex+1);
+		return spareFrame.getScore() + nextFrame.getFirstThrow();
+	}
+	
+	private int getTotalScoreForStrike(Frame strikeFrame, int frameIndex) {
+		if(this.isLastFrame(frameIndex)) {
+			return strikeFrame.getScore() + strikeFrame.getSecondThrow() + strikeFrame.getBonusThrow();
+		}
+		Frame nextFrame = frames.get(frameIndex+1);
+		if(nextFrame instanceof StrikeFrame && !isLastFrame(frameIndex+1)) {
+			Frame afterNextFrame = frames.get(frameIndex+1);
+			return strikeFrame.getScore() + nextFrame.getScore() + afterNextFrame.getFirstThrow();
+		} else {
+			return strikeFrame.getScore() + nextFrame.getFirstThrow() + nextFrame.getSecondThrow();
+		}
+	}
+	
+	private boolean isLastFrame(int frameNumber) {
+		return FRAMES_PER_GAME == frameNumber+1;
+	}
 
 }
--- BowlingKata/src/test/java/de/sebmey/kata/bowling/BowlingTest.java
+++ BowlingKata/src/test/java/de/sebmey/kata/bowling/BowlingTest.java
@@ -2,13 +2,62 @@ package de.sebmey.kata.bowling;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class BowlingTest {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBowlingScoreWithPerfectGame() {
+		final String perfectGame = "X X X X X X X X X XXX";
+		final int expectedScore = 300;
+		Bowling game = new Bowling(perfectGame);
+		assertNotNull(game);
+		assertEquals(expectedScore, game.calculateScore());
 	}
-
+	
+	@Test
+	public void testBowlingScoreWithTwoStrikesAndTwoSpares() {
+		final String gameWithTwoStrikesAndTwoSpares = "-- X 6/ 71 6/ X 81 -- 21 44";
+		final int expectedScore = 104;
+		Bowling game = new Bowling(gameWithTwoStrikesAndTwoSpares);
+		assertNotNull(game);
+		assertEquals(expectedScore, game.calculateScore());
+	}
+	
+	@Test
+	public void testBowlingScoreTenPairsOfNineAndMiss() {
+		final String tenPairsOfNineAndMiss = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
+		final int expectedScore = 90;
+		Bowling game = new Bowling(tenPairsOfNineAndMiss);
+		assertNotNull(game);
+		assertEquals(expectedScore, game.calculateScore());
+	}
+	
+	@Test
+	public void testBowlingScoreAllThrowsFivePins() {
+		final String allThrowsFivePins = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5";
+		final int expectedScore = 150;
+		Bowling game = new Bowling(allThrowsFivePins);
+		assertNotNull(game);
+		assertEquals(expectedScore, game.calculateScore());
+	}
+	
+	@Test
+	public void testBowlingGameWithNonFullGameInput() {
+		final String fiveFrames = "-- 11 22 33 44";
+		Bowling game = new Bowling(fiveFrames);
+		thrown.expect(IndexOutOfBoundsException.class);
+		game.calculateScore();
+	}
+	
+	@Test
+	public void testBowlingGameWithEmptyInput() {
+		thrown.expect(IllegalArgumentException.class);
+		new Bowling(null);
+	}
 }
