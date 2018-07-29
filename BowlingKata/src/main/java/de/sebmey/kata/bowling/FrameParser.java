8c215bb69e56bf800c089a98d511693fd4e9bda4
Vivec <meyer_sebastian@outlook.com>
2018-07-29 13:21:05 +0200

    First implementation for FrameParser

--- BowlingKata/src/main/java/de/sebmey/kata/bowling/FrameParser.java
+++ BowlingKata/src/main/java/de/sebmey/kata/bowling/FrameParser.java
@@ -1,17 +1,38 @@
 package de.sebmey.kata.bowling;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FrameParser {
 
+	public static final String DELIMITER = " ";
+	
 	public static Frame parse(String singleFrame) {
-		// TODO Auto-generated method stub
-		return null;
+		if(StandardFrame.checkInputValidity(singleFrame)) {
+			return new StandardFrame(singleFrame);
+		} else if(SpareFrame.checkInputValidity(singleFrame)) {
+			return new SpareFrame(singleFrame);
+		} else if(StrikeFrame.checkInputValidity(singleFrame)) {
+			return new StrikeFrame(singleFrame);
+		} else {
+			throw new IllegalArgumentException("Cannot find a frame to parse this input in. Please provide valid input.");
+		}
 	}
 
 	public static List<Frame> parseLine(String lineOfFrames) {
-		// TODO Auto-generated method stub
-		return null;
+		if(lineOfFrames == null) {
+			throw new IllegalArgumentException("The input to parse cannot be null.");
+		}
+		
+		String[] frames = lineOfFrames.split(DELIMITER);
+		List<Frame> parseResult = new ArrayList<Frame>();
+		
+		for(String frame : frames) {
+			Frame parsedFrame = FrameParser.parse(frame);
+			parseResult.add(parsedFrame);
+		}
+		
+		return parseResult;
 	}
 
 }
