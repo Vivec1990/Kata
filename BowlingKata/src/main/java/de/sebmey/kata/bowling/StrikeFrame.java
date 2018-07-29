f7247d54a3806bf401a8ba38f02cff33886b2eee
Vivec <meyer_sebastian@outlook.com>
2018-07-29 12:40:43 +0200

    Basic Implementation of StrikeFrame and Test

--- BowlingKata/src/main/java/de/sebmey/kata/bowling/StrikeFrame.java
+++ BowlingKata/src/main/java/de/sebmey/kata/bowling/StrikeFrame.java
@@ -1,5 +1,59 @@
 package de.sebmey.kata.bowling;
 
-public class StrikeFrame {
+public class StrikeFrame implements Frame {
+
+	private static final int SCORE = 10;
+	private static final int LOOKAHEAD_THROWS = 1;
+	
+	private int firstThrow;	
+	private int bonusThrow;
+	private int secondBonusThrow;
+	
+	public StrikeFrame(String strikeFrameInput) {
+		if(!checkInputValidity(strikeFrameInput)) {
+			throw new IllegalArgumentException("The input is invalid to create a strike frame. Please make sure it is exactly 2 characters long and only contains characters from this list: " + VALID_INPUT_CHARACTERS);
+		}
+		this.firstThrow = Frame.parseCharacterIntoScore(strikeFrameInput.charAt(0));
+		this.secondBonusThrow = 10 - this.firstThrow;
+		if(strikeFrameInput.length() == 3) {
+			this.bonusThrow = Frame.parseCharacterIntoScore(strikeFrameInput.charAt(2));
+		}
+	}
+	
+	@Override
+	public int getScore() {
+		return SCORE;
+	}
+
+	public int getFirstThrow() {
+		return firstThrow;
+	}
+
+	public int getSecondThrow() {
+		return secondBonusThrow;
+	}
+
+	public int getBonusThrow() {
+		return bonusThrow;
+	}
+	
+	@Override
+	public int getLookAheadThrows() {
+		return LOOKAHEAD_THROWS;
+	}
+
+	@Override
+	public boolean checkInputValidity(String input) {
+		if(input == null || input.length() < 1 || input.length() > 3) {
+			return false;
+		}
+		if(input.charAt(0) != Frame.STRIKE) {
+			return false;
+		}
+		if(input.length() == 3 && input.charAt(1) != Frame.STRIKE) {
+			return false;
+		}
+		return true;
+	}
 	
 }
--- BowlingKata/src/test/java/de/sebmey/kata/bowling/StrikeFrameTest.java
+++ BowlingKata/src/test/java/de/sebmey/kata/bowling/StrikeFrameTest.java
@@ -2,13 +2,60 @@ package de.sebmey.kata.bowling;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StrikeFrameTest {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStrikeFrameCreationWithValidInput() {
+		final int expectedScore = 10;
+		
+		StrikeFrame strikeFrame = new StrikeFrame(Frame.STRIKE+"");
+		assertEquals(expectedScore, strikeFrame.getScore());
 	}
-
+	
+	@Test
+	public void testStrikeFrameCreationWithInvalidInput() {
+		final String invalidStrikeFrameInput = "u";
+		thrown.expect(IllegalArgumentException.class);
+		new StrikeFrame(invalidStrikeFrameInput);
+	}
+	
+	@Test
+	public void testStrikeFrameCreationWithNonStrikeFrameInput() {
+		final String standardFrameInput = "44";
+		
+		StandardFrame frame = new StandardFrame(standardFrameInput);
+		
+		final int expectedFirstThrow = 4;
+		final int expectedSecondThrow = 4;
+		final int expectedScore = 8;
+		
+		assertNotNull(frame);
+		assertEquals(expectedFirstThrow, frame.getFirstThrow());
+		assertEquals(expectedSecondThrow, frame.getSecondThrow());
+		assertEquals(expectedScore, frame.getScore());
+		
+		thrown.expect(IllegalArgumentException.class);
+		new StrikeFrame(standardFrameInput);
+	}
+	
+	@Test
+	public void testStrikeFrameCreationWithEmptyInput() {
+		thrown.expect(IllegalArgumentException.class);
+		new StrikeFrame(null);
+	}
+	
+	@Test
+	public void testStrikeFrameCreationWithWrongInputLength() {
+		final String wrongStrikeFrameInputLength = "X45678";
+		thrown.expect(IllegalArgumentException.class);
+		new StrikeFrame(wrongStrikeFrameInputLength);
+	}
+	
 }
