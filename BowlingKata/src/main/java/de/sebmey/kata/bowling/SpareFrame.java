006dfb41ded76209049a2922912483b564cd0162
Vivec <meyer_sebastian@outlook.com>
2018-07-29 12:18:23 +0200

    Basic Implementation of SpareFrame and Test

--- BowlingKata/src/main/java/de/sebmey/kata/bowling/SpareFrame.java
+++ BowlingKata/src/main/java/de/sebmey/kata/bowling/SpareFrame.java
@@ -1,5 +1,56 @@
 package de.sebmey.kata.bowling;
 
-public class SpareFrame {
+public class SpareFrame implements Frame {
+	
+	private static final int SCORE = 10;
+	private static final int LOOKAHEAD_THROWS = 1;
+	
+	private int firstThrow;
+	private int secondThrow;
+	private int bonusThrow;
+	
+	public SpareFrame(String spareFrameInput) {
+		if(!checkInputValidity(spareFrameInput)) {
+			throw new IllegalArgumentException("The input is invalid to create a standard frame. Please make sure it is exactly 2 characters long and only contains characters from this list: " + VALID_INPUT_CHARACTERS);
+		}
+		this.firstThrow = Frame.parseCharacterIntoScore(spareFrameInput.charAt(0));
+		this.secondThrow = 10 - this.firstThrow;
+		if(spareFrameInput.length() == 3) {
+			this.bonusThrow = Frame.parseCharacterIntoScore(spareFrameInput.charAt(2));
+		}
+	}
+	
+	@Override
+	public int getScore() {
+		return SCORE;
+	}
 
+	public int getFirstThrow() {
+		return firstThrow;
+	}
+
+	public int getSecondThrow() {
+		return secondThrow;
+	}
+
+	public int getBonusThrow() {
+		return bonusThrow;
+	}
+	
+	@Override
+	public int getLookAheadThrows() {
+		return LOOKAHEAD_THROWS;
+	}
+
+	@Override
+	public boolean checkInputValidity(String input) {
+		if(input == null || input.length() < 2 || input.length() > 3) {
+			return false;
+		}
+		if(input.charAt(1) != Frame.SPARE) {
+			return false;
+		}
+		return true;
+	}
+	
 }
--- BowlingKata/src/test/java/de/sebmey/kata/bowling/SpareFrameTest.java
+++ BowlingKata/src/test/java/de/sebmey/kata/bowling/SpareFrameTest.java
@@ -2,13 +2,56 @@ package de.sebmey.kata.bowling;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class SpareFrameTest {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSpareFrameCreationWithValidInput() {
+		final String validSpareFrameInput = "4/";
+		SpareFrame spareFrame = new SpareFrame(validSpareFrameInput);
+		
+		assertNotNull(spareFrame);
+		
+		final int expectedFirstThrowValue = 4;
+		final int expectedSecondThrowValue = 6;
+		final int expectedScore = 10;
+		
+		assertEquals(expectedFirstThrowValue, spareFrame.getFirstThrow());
+		assertEquals(expectedSecondThrowValue, spareFrame.getSecondThrow());
+		assertEquals(expectedScore, spareFrame.getScore());
+	}
+	
+	@Test
+	public void testSpareFrameCreationWithInvalidInput() {
+		final String invalidSpareFrameInput = "4p";
+		thrown.expect(IllegalArgumentException.class);
+		new SpareFrame(invalidSpareFrameInput);
+	}
+	
+	@Test
+	public void testSpareFrameCreationWithEmptyInput() {
+		thrown.expect(IllegalArgumentException.class);
+		new SpareFrame(null);
+	}
+	
+	@Test
+	public void testSpareFrameCreationWithNonSpareFrameInput() {
+		final String nonSpareFrameInput = "44";
+		thrown.expect(IllegalArgumentException.class);
+		new SpareFrame(nonSpareFrameInput);
+	}
+	
+	@Test
+	public void testSpareFrameCreatioNWithInvalidInputLength() {
+		final String tooShortInput = "/";
+		thrown.expect(IllegalArgumentException.class);
+		new SpareFrame(tooShortInput);
 	}
 
 }
